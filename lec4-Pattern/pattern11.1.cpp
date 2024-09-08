94297154331ff1a37211d157a51067af4e54c887
Moinul Islam <moinulislam159632@gmail.com>
2024-09-09 01:48:33 +0600

    completed lecture4 - Patterns

---
+++ lec4-Pattern/pattern11.1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i+1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < 2*(n-i-1); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i+1; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+    for (int i = n-1; i >= 0; i--)
+    {
+        for (int j = 0; j < i+1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < 2*(n-i-1); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i+1; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
